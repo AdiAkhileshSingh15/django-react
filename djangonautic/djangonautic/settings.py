7cd4d3abc4ee07d78e46e28ffe33827301b44bb5
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-05-12 12:13:13 +0530

    3rd

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-r@7#4it8g(eccie)a2)l(9b)0v2ldq3e5m-xu@eg7^b$di6vp3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["https://adiakhileshsingh15.github.io/django-react/"]
+ALLOWED_HOSTS = ["192.168.0.74", "djangoapi.herokuapp.com"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -123,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = []
 
